4528ab2c4dbd9837a66f30f5757e8c182bb4b910
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-07-29 14:05:41 -0500

    fix conversion system

--- src/utils/kelvinToFarenheit.js
+++ src/utils/kelvinToFarenheit.js
@@ -3,5 +3,5 @@ export const kelvinToFarenheit = (kelvinDegrades) => {
     const celsius = parseFloat(kelvinToCelsius(kelvinDegrades));
     const farenheitConversion = 9/5;
     const farenheitConst = 32;
-    return (celsius + farenheitConversion) + farenheitConst;
+    return (celsius * farenheitConversion) + farenheitConst;
 }
